b500a7166f8ce34025bd1591e8a8c0e5b42b0879
M G Asheque Mahmood <mahmood.asheque@gmail.com>
2023-06-05 19:06:35 +0300

    initial setup

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "../Components/Header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+import { Col, Container, Row } from "react-bootstrap";
+import LeftNav from "../Components/LeftNav/LeftNav";
+import RightNav from "../Components/RightNav/RightNav";
+
+const Main = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+      <Row>
+        <Col lg={3}>
+            <LeftNav></LeftNav>
+        </Col>
+        <Col lg={6}>Main content</Col>
+        <Col lg={3}>
+            <RightNav></RightNav>
+        </Col>
+      </Row>
+    </Container>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn7mc6TM7MrTmRyE8IZIQ3JoohPEpjBR8",
+  authDomain: "dragon-news-83464.firebaseapp.com",
+  projectId: "dragon-news-83464",
+  storageBucket: "dragon-news-83464.appspot.com",
+  messagingSenderId: "245474668175",
+  appId: "1:245474668175:web:dd4fdac40e82c952058afa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
